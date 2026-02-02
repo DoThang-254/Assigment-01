bd253e04e63453dbc38c9542b101c0c8bb34fb05
DoThang-254 <thangmoneo2542004@gmail.com>
2026-02-02 22:11:09 +0700

    last v1

--- DoQuangThang_SE1885_A01/DoQuangThang_SE1885_A01_BE/BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ DoQuangThang_SE1885_A01/DoQuangThang_SE1885_A01_BE/BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca7842fb6745d1c39c3806a1d2c9f4c8877c488")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DoQuangThang_SE1885_A01/DoQuangThang_SE1885_A01_BE/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DoQuangThang_SE1885_A01/DoQuangThang_SE1885_A01_BE/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca7842fb6745d1c39c3806a1d2c9f4c8877c488")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
